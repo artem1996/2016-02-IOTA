81862146496c8d8f879153fec4fc556f52601458
artem1996 <shishkinaa96@gmail.com>
2016-04-21 18:17:02 +0300

    up

--- src/test/java/ru/cdecl/pub/iota/services/AccountServiceJdbiImplTest.java
+++ src/test/java/ru/cdecl/pub/iota/services/AccountServiceJdbiImplTest.java
@@ -1,23 +1,13 @@
 package ru.cdecl.pub.iota.services;
 
-import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.cdecl.pub.iota.models.UserProfile;
 import static org.junit.Assert.*;
-import ru.cdecl.pub.iota.services.AccountServiceJdbiImpl;
 import ru.cdecl.pub.iota.main.DataSourceFactory;
-import ru.cdecl.pub.iota.servlets.UserServlet;
-import static org.mockito.Mockito.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.StringWriter;
-import java.lang.String;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 public class AccountServiceJdbiImplTest extends AccountServiceTest {
 
--- src/test/java/ru/cdecl/pub/iota/services/AccountServiceTest.java
+++ src/test/java/ru/cdecl/pub/iota/services/AccountServiceTest.java
@@ -1,22 +1,7 @@
 package ru.cdecl.pub.iota.services;
 
-import junit.framework.Assert;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import ru.cdecl.pub.iota.models.UserProfile;
-import static org.junit.Assert.*;
-import ru.cdecl.pub.iota.services.AccountServiceJdbiImpl;
-import ru.cdecl.pub.iota.main.DataSourceFactory;
-import ru.cdecl.pub.iota.servlets.UserServlet;
-import static org.mockito.Mockito.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
-import java.io.StringWriter;
-import java.lang.String;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 public abstract class AccountServiceTest {
     public UserProfile user;
